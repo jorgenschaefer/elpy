3dcdf65f156900ffde8bb05c54e2297d9859e98f
Jorgen Schaefer <forcer@forcix.cx>
2013-02-09 17:36:59 +0100

    setup.py: Don't error if README.txt exists, just don't write it.
    This caused a problem on pypi installation. This also meant we
    had to upload 0.8.1 there instead of 0.8.

--- setup.py
+++ setup.py
@@ -5,15 +5,13 @@ from distutils.core import setup
 import os
 import elpy
 
-if os.path.exists("README.txt"):
-    exit("README.txt exists, will be clobbered.")
-
-with open("README.txt", "w") as f:
-    f.write(elpy.__doc__)
+if not os.path.exists("README.txt"):
+    with open("README.txt", "w") as f:
+        f.write(elpy.__doc__)
 
 try:
     setup(name="elpy",
-          version=elpy.__version__,
+          version=elpy.__version__ + ".1",
           description="Backend for the elpy Emacs mode",
           author="Jorgen Schaefer",
           author_email="forcer@forcix.cx",
