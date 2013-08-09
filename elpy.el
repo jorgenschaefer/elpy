0c75fbef26d7690721a784b6ae98525c9044bac1
Jorgen Schaefer <forcer@forcix.cx>
2013-08-09 18:21:56 +0200

    Bump version to 1.1

--- elpy.el
+++ elpy.el
@@ -4,7 +4,7 @@
 
 ;; Author: Jorgen Schaefer <forcer@forcix.cx>
 ;; URL: https://github.com/jorgenschaefer/elpy
-;; Version: 1.0
+;; Version: 1.1
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License
