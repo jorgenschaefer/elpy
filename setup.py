418c619f86628c55c7835a6e0da05de100a77281
Jorgen Schaefer <forcer@forcix.cx>
2013-09-16 23:00:37 +0200

    setup.py: Depend on flake8 >= 2.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 import os
 import elpy
@@ -10,22 +10,23 @@ if not os.path.exists("README.txt"):
         f.write(elpy.__doc__)
 
 try:
-    setup(name="elpy",
-          version=elpy.__version__,
-          description="Backend for the elpy Emacs mode",
-          author="Jorgen Schaefer",
-          author_email="forcer@forcix.cx",
-          url="https://github.com/jorgenschaefer/elpy",
-          license="GPL",
-          packages=["elpy", "elpy.backends", "elpy.utils"],
-          data_files=[('elpy', ["LICENSE"])],
-          classifiers=[
-              "Development Status :: 5 - Production/Stable",
-              ("License :: OSI Approved :: "
-               "GNU General Public License v3 or later (GPLv3+)"),
-              "Topic :: Text Editors :: Emacs",
-          ],
-          requires=["flake8"]
+    setup(
+        name="elpy",
+        version=elpy.__version__,
+        description="Backend for the elpy Emacs mode",
+        author="Jorgen Schaefer",
+        author_email="forcer@forcix.cx",
+        url="https://github.com/jorgenschaefer/elpy",
+        license="GPL",
+        packages=find_packages(),
+        data_files=[('elpy', ["LICENSE"])],
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            ("License :: OSI Approved :: "
+             "GNU General Public License v3 or later (GPLv3+)"),
+            "Topic :: Text Editors :: Emacs",
+        ],
+        install_requires=["flake8>=2.0"],
     )
 finally:
     os.unlink("README.txt")
