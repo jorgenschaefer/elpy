d5051daede905ff974c6d8f2c8e291cd6e20bba9
Jorgen Schaefer <forcer@forcix.cx>
2013-09-20 18:54:20 +0200

    Fix MANIFEST.in
    ... hat tip to Hynek at
    http://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/

--- setup.py
+++ setup.py
@@ -2,35 +2,26 @@
 
 from setuptools import setup, find_packages
 
-import os
 import elpy
 
-if not os.path.exists("README.txt"):
-    with open("README.txt", "w") as f:
-        f.write(elpy.__doc__)
-
-try:
-    setup(
-        name="elpy",
-        version=elpy.__version__,
-        description="Backend for the elpy Emacs mode",
-        author="Jorgen Schaefer",
-        author_email="forcer@forcix.cx",
-        url="https://github.com/jorgenschaefer/elpy",
-        license="GPL",
-        packages=find_packages(),
-        data_files=[('elpy', ["LICENSE"])],
-        classifiers=[
-            "Development Status :: 5 - Production/Stable",
-            ("License :: OSI Approved :: "
-             "GNU General Public License v3 or later (GPLv3+)"),
-            "Topic :: Text Editors :: Emacs",
-        ],
-        install_requires=["flake8>=2.0"],
-    )
-finally:
-    os.unlink("README.txt")
-    try:
-        os.unlink("MANIFEST")
-    except OSError:
-        pass
+setup(
+    name="elpy",
+    version=elpy.__version__,
+    description="Backend for the elpy Emacs mode",
+    long_description=elpy.__doc__,
+    url="https://github.com/jorgenschaefer/elpy",
+    license="GPL",
+    author="Jorgen Schaefer",
+    author_email="forcer@forcix.cx",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        ("License :: OSI Approved :: "
+         "GNU General Public License v3 or later (GPLv3+)"),
+        "Topic :: Text Editors :: Emacs",
+    ],
+    install_requires=["flake8>=2.0"],
+)
