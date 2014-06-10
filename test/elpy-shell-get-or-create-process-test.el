d866b5a324a1ded07e55adabbb07f4a4216fa472
Jorgen Schaefer <contact@jorgenschaefer.de>
2014-06-10 20:02:40 +0200

    Fix test problem in Emacs 24.1 due to zombie buffer.

--- test/elpy-shell-get-or-create-process-test.el
+++ test/elpy-shell-get-or-create-process-test.el
@@ -1,5 +1,9 @@
 (ert-deftest elpy-shell-get-or-create-process-should-return-process ()
   (elpy-testcase ()
+    ;; This was left set by some other test, apparently, which breaks
+    ;; in 24.1.
+    (setq python-buffer nil)
+
     (let ((proc (elpy-shell-get-or-create-process)))
       (with-current-buffer (process-buffer proc)
         (should (eq major-mode 'inferior-python-mode))))))
