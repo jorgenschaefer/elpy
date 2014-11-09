7fe300cd2979abb3c0b7b58dd31b04ed276b9ca6
Georg Brandl <georg@python.org>
2014-11-09 11:56:46 +0100

    Fix tests with newest importmagic HEAD, and simplify add-import
    * we can now give the blacklist_re argument directly without monkeypatch
    * add-import now works with suggested import statements instead of a "from module"
      which does not always apply
    * fix the default "symbol to import" suggestion

--- test/elpy-importmagic-add-import-test.el
+++ test/elpy-importmagic-add-import-test.el
@@ -6,5 +6,5 @@ import os
 print()
 ")
     (when (gethash "importmagic_version" (elpy-config--get-config))
-      (elpy-importmagic-add-import "mysymbol" "mymodule")
+      (elpy-importmagic-add-import "from mymodule import mysymbol")
       (should (s-contains-p "from mymodule import mysymbol\n" (buffer-string))))))
