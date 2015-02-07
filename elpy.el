98db669273f466de08c345f03b54602871aedaa1
Jorgen Schaefer <contact@jorgenschaefer.de>
2015-02-07 09:39:17 +0100

    Bump version: 1.7.0 → 1.7.50

--- elpy.el
+++ elpy.el
@@ -4,7 +4,7 @@
 
 ;; Author: Jorgen Schaefer <contact@jorgenschaefer.de>
 ;; URL: https://github.com/jorgenschaefer/elpy
-;; Version: 1.7.0
+;; Version: 1.7.50
 ;; Keywords: Python, IDE, Languages, Tools
 ;; Package-Requires: ((company "0.8.2") (find-file-in-project "3.3")  (highlight-indentation "0.5.0") (pyvenv "1.3") (yasnippet "0.8.0"))
 
@@ -80,7 +80,7 @@
 (require 'elpy-refactor)
 (require 'pyvenv)
 
-(defconst elpy-version "1.7.0"
+(defconst elpy-version "1.7.50"
   "The version of the Elpy lisp code.")
 
 ;;;;;;;;;;;;;;;;;;;;;;
--- elpy/__init__.py
+++ elpy/__init__.py
@@ -37,5 +37,5 @@ This package is unlikely to be useful on its own.
 """
 
 __author__ = "Jorgen Schaefer"
-__version__ = "1.7.0"
+__version__ = "1.7.50"
 __license__ = "GPL"
