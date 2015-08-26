c33f30fa2709d252cb6a2bd89fc5d36da171bb4e
ChillarAnand <anand21nanda@gmail.com>
2015-08-26 19:12:00 +0530

    Ensure autopep8 is installed before fixing code

--- elpy/auto_pep8.py
+++ elpy/auto_pep8.py
@@ -1,6 +1,10 @@
 """Glue for the "autopep8" library.
 
 """
+
+from elpy.rpc import Fault
+
+
 try:
     import autopep8
 except ImportError:  # pragma: no cover
@@ -11,4 +15,7 @@ def fix_code(code):
     """Formats Python code to conform to the PEP 8 style guide.
 
     """
+    if not autopep8:
+        raise Fault('autopep8 not installed, cannot fix code.',
+                    code=400)
     return autopep8.fix_code(code)
