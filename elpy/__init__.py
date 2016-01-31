ca7365f0d1cf36d36b9c3f862c644406172286e0
Jorgen Schaefer <contact@jorgenschaefer.de>
2016-01-31 13:50:07 +0100

    Update copyright lines.

--- elpy/__init__.py
+++ elpy/__init__.py
@@ -1,6 +1,6 @@
 # Elpy, the Emacs Lisp Python Environment
 
-# Copyright (C) 2013 Jorgen Schaefer
+# Copyright (C) 2013-2016 Jorgen Schaefer
 
 # Author: Jorgen Schaefer <contact@jorgenschaefer.de>
 # URL: http://github.com/jorgenschaefer/elpy
