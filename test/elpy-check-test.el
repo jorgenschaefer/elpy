6f8a6f522f8e3d1597f59c7a419c84edb56237fc
anand21nanda@gmail.com <anand21nanda@gmail.com>
2016-05-26 20:51:19 +0530

    Use extra arguments depending on lint checker
    Almost all lint checkers(pyflakes, flake8, pep8) use `--exclude`
    argument to exclude specified files, but pylint uses `--ignore`
    argument.

--- test/elpy-check-test.el
+++ test/elpy-check-test.el
@@ -33,6 +33,19 @@
       (should (equal command (format "%s %s --exclude=foo,bar"
                                      python-check-command
                                      project-root))))))
+(ert-deftest elpy-check-should-pass-extra-args-for-project-check-with-pylint ()
+  (elpy-testcase ((:project project-root "test.py"))
+                 (find-file (f-join project-root "test.py"))
+                 (mletf* ((command nil)
+                          (python-check-command "pylint")
+                          (elpy-project-root project-root)
+                          (elpy-project-ignored-directories '("foo" "bar"))
+                          (compilation-start (command-arg mode name-function)
+                                             (setq command command-arg)))
+                         (elpy-check t)
+                         (should (equal command (format "%s %s --ignore=foo,bar"
+                                                        python-check-command
+                                                        project-root))))))
 
 (ert-deftest elpy-check-should-check-file-if-no-project-root ()
   (elpy-testcase ((:project project-root "test.py"))
