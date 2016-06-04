132a202f9a6e62742f81d73e56eec6842672391e
anand21nanda@gmail.com <anand21nanda@gmail.com>
2016-06-04 22:29:01 +0530

    Improve cursor positioning while format
    Deactivate mark after formating a region. Forward cursor only if entire
    buffer is formated.

--- test/elpy-yapf-fix-code-test.el
+++ test/elpy-yapf-fix-code-test.el
@@ -15,3 +15,27 @@
                       (buffer-be
                        "x = 1"
                        "y = 2_|_"))))))
+
+(ert-deftest elpy-yapf-fix-code-in-region-should-retain-line-and-column ()
+  (let* ((pyversion (getenv "TRAVIS_PYTHON_VERSION"))
+         (yapf-not-supported (or (string< pyversion "2.7")
+                                 (and (not (string< pyversion "3.0"))
+                                      (string< pyversion "3.4")))))
+    (unless yapf-not-supported
+      (elpy-testcase ()
+                     (set-buffer-string-with-point
+                      "_|_y =  2"
+                      "z = 3"
+                      "x = 3"
+                      )
+                     (elpy/mark-region (point) 13)
+
+                     (elpy-yapf-fix-code)
+
+                     (should (not mark-active))
+                     (should
+                      (buffer-be
+                       "y = 2"
+                       "z = 3"
+                       "_|_x = 3"
+                       ))))))
