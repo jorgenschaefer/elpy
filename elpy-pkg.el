1976b2010d237573e981fd46f769c6c1f866d4e8
Jorgen Schaefer <contact@jorgenschaefer.de>
2017-02-12 12:01:20 +0000

    Update company dependency to 0.9.2

--- elpy-pkg.el
+++ elpy-pkg.el
@@ -1,6 +1,6 @@
 (define-package "elpy" "1.14.1"
                 "Emacs Python Development Environment"
-                '((company "0.8.2")
+                '((company "0.9.2")
                   (find-file-in-project "3.3")
                   (highlight-indentation "0.5.0")
                   (pyvenv "1.3")
