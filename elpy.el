72f06e3e7f5010ff794e0db73950b33bcdfb2e72
Jorgen Schaefer <contact@jorgenschaefer.de>
2017-02-24 15:01:54 +0000

    Add workaround to bug in Emacs before 25.2
    Fixes #887

--- elpy.el
+++ elpy.el
@@ -3959,6 +3959,16 @@ which we're looking."
            highlight-indent-active)
       (highlight-indentation)))))
 
+;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=25753#44
+(when (version< emacs-version "25.2")
+  (defun python-shell-completion-native-try ()
+    "Return non-nil if can trigger native completion."
+    (let ((python-shell-completion-native-enable t)
+          (python-shell-completion-native-output-timeout
+           python-shell-completion-native-try-output-timeout))
+      (python-shell-completion-native-get-completions
+       (get-buffer-process (current-buffer))
+       nil "_"))))
 
 (provide 'elpy)
 ;;; elpy.el ends here
