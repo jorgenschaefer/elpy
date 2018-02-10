2d58fdf6c740d6a86aa7daf9745e703675931534
galaunay <gaby.launay@tutanota.com>
2018-02-10 22:29:59 +0000

    Fix django don't appearing in elpy-config subgroup

--- elpy-django.el
+++ elpy-django.el
@@ -34,21 +34,21 @@
 Best to set it to full path to 'manage.py' if it's available."
   :type 'string
   :safe 'stringp
-  :group 'elpy)
+  :group 'elpy-django)
 (make-variable-buffer-local 'elpy-django-command)
 
 (defcustom elpy-django-server-ipaddr "127.0.0.1"
   "What address Django will use when running the dev server."
   :type 'string
   :safe 'stringp
-  :group 'elpy)
+  :group 'elpy-django)
 (make-variable-buffer-local 'elpy-django-server-ipaddr)
 
 (defcustom elpy-django-server-port "8000"
   "What port Django will use when running the dev server."
   :type 'string
   :safe 'stringp
-  :group 'elpy)
+  :group 'elpy-django)
 (make-variable-buffer-local 'elpy-django-server-port)
 
 (defcustom elpy-django-server-command "runserver"
@@ -56,7 +56,7 @@ Best to set it to full path to 'manage.py' if it's available."
 command to use."
   :type 'string
   :safe 'stringp
-  :group 'elpy)
+  :group 'elpy-django)
 (make-variable-buffer-local 'elpy-django-server-command)
 
 (defcustom elpy-django-always-prompt nil
@@ -64,7 +64,7 @@ command to use."
 to pass with the chosen command."
   :type 'boolean
   :safe 'booleanp
-  :group 'elpy)
+  :group 'elpy-django)
 (make-variable-buffer-local 'elpy-django-always-prompt)
 
 (defcustom elpy-django-commands-with-req-arg '("startapp" "startproject"
@@ -75,7 +75,7 @@ to pass with the chosen command."
 require arguments in order for it to work."
   :type 'list
   :safe 'listp
-  :group 'elpy)
+  :group 'elpy-django)
 (make-variable-buffer-local 'elpy-django-commands-with-req-arg)
 
 
@@ -205,7 +205,7 @@ When called with a prefix (C-u), it will prompt for additional args."
 
 (define-minor-mode elpy-django
   "Minor mode to for Django commands."
-  :group 'elpy)
+  :group 'elpy-django)
 
 (provide 'elpy-django)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
