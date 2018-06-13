c9e612ad34b0206a1c6ed3388578c05f5bfc8da0
galaunay <gaby.launay@tutanota.com>
2018-06-13 22:44:20 +0100

    Try to fix flymake test for emacs >= 26.1

--- test/elpy-flymake-show-error-test.el
+++ test/elpy-flymake-show-error-test.el
@@ -1,13 +1,29 @@
 (ert-deftest elpy-flymake-show-error ()
-  ;; not used for emacs >= 26.1
-  (when (version< emacs-version "26.1")
-      (elpy-testcase ()
+  (elpy-testcase ()
+       ;; Emacs < 26.1
+       (when (version< emacs-version "26.1")
         (mletf* ((flymake-find-err-info
                   (info line)
                   (list (list (flymake-ler-make-ler "" "" "" "test-error-1")
                               (flymake-ler-make-ler "" "" "" "test-error-2"))))
                  (output nil)
-                 (message (fmt &rest args) (setq output (apply 'format fmt args))))
+                 (message (fmt &rest args)
+                          (setq output (apply 'format fmt args))))
+
+          (elpy-flymake-show-error)
+
+          (should (equal output "test-error-1, test-error-2"))))
+       (when (version<= "26.1" emacs-version)
+
+         (mletf* ((flymake-diagnostics
+                   (point)
+                   (list (flymake-make-diagnostic (current-buffer)
+                          23 56 :error "test-error-1")
+                         (flymake-make-diagnostic (current-buffer)
+                          62 28 :error "test-error-2")))
+                   (output nil)
+                   (message (fmt &rest args)
+                            (setq output (apply 'format fmt args))))
 
           (elpy-flymake-show-error)
 
