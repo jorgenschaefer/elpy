2904e33fb77c8e6de3b82e8c8f6e0ee1e9aba312
galaunay <gaby.launay@tutanota.com>
2018-06-13 21:06:33 +0100

    Add emacs 26.1 to travis

--- test/elpy-flymake-show-error-test.el
+++ test/elpy-flymake-show-error-test.el
@@ -1,12 +1,14 @@
 (ert-deftest elpy-flymake-show-error ()
-  (elpy-testcase ()
-    (mletf* ((flymake-find-err-info
-              (info line)
-              (list (list (flymake-ler-make-ler "" "" "" "test-error-1")
-                          (flymake-ler-make-ler "" "" "" "test-error-2"))))
-             (output nil)
-             (message (fmt &rest args) (setq output (apply 'format fmt args))))
+  ;; not used for emacs >= 26.1
+  (when (version< emacs-version "26.1")
+      (elpy-testcase ()
+        (mletf* ((flymake-find-err-info
+                  (info line)
+                  (list (list (flymake-ler-make-ler "" "" "" "test-error-1")
+                              (flymake-ler-make-ler "" "" "" "test-error-2"))))
+                 (output nil)
+                 (message (fmt &rest args) (setq output (apply 'format fmt args))))
 
-      (elpy-flymake-show-error)
+          (elpy-flymake-show-error)
 
-      (should (equal output "test-error-1, test-error-2")))))
+          (should (equal output "test-error-1, test-error-2"))))))
