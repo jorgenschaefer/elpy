0f972ae5cbc5824d9dcf2803561bdc845a81937b
Jorgen Schaefer <contact@jorgenschaefer.de>
2018-10-05 13:55:08 +0000

    Bump version: 1.24.0 → 1.25.0

--- elpy-pkg.el
+++ elpy-pkg.el
@@ -1,4 +1,4 @@
-(define-package "elpy" "1.24.0"
+(define-package "elpy" "1.25.0"
                 "Emacs Python Development Environment"
                 '((company "0.9.2")
                   (emacs "24.4")
