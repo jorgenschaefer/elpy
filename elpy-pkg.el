85f3a1c20f8c5a62bf917dab6b6fba75353ffb07
Jorgen Schaefer <contact@jorgenschaefer.de>
2018-12-02 16:06:41 +0000

    Bump version: 1.26.0 → 1.27.0

--- elpy-pkg.el
+++ elpy-pkg.el
@@ -1,4 +1,4 @@
-(define-package "elpy" "1.26.0"
+(define-package "elpy" "1.27.0"
                 "Emacs Python Development Environment"
                 '((company "0.9.2")
                   (emacs "24.4")
--- elpy/__init__.py
+++ elpy/__init__.py
@@ -37,5 +37,5 @@ This package is unlikely to be useful on its own.
 """
 
 __author__ = "Jorgen Schaefer"
-__version__ = "1.26.0"
+__version__ = "1.27.0"
 __license__ = "GPL"
