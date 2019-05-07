390b90cbdec22234b0f007fa02593e68b7937e94
galaunay <gaby.launay@tutanota.com>
2019-05-07 21:48:41 +0200

    Don't send statement when there is no statement to send

--- elpy-shell.el
+++ elpy-shell.el
@@ -808,17 +808,17 @@ Otherwise, skips forward to the next code line and sends the
 corresponding statement."
   (interactive)
   (elpy-shell--ensure-shell-running)
-  (when (not elpy-shell-echo-input) (elpy-shell--append-to-shell-output "\n"))
-  (let ((beg (progn (elpy-shell--nav-beginning-of-statement)
-                    (save-excursion
-                      (beginning-of-line)
-                      (point))))
-        (end (progn (elpy-shell--nav-end-of-statement) (point))))
-    (unless (eq beg end)
-      (elpy-shell--flash-and-message-region beg end)
+  (elpy-shell--nav-beginning-of-statement)
+  ;; Make sure there is a statement to send
+  (when (not (looking-at "[[:space:]]*$"))
+    (when (not elpy-shell-echo-input) (elpy-shell--append-to-shell-output "\n"))
+    (let ((beg (save-excursion (beginning-of-line) (point)))
+          (end (progn (elpy-shell--nav-end-of-statement) (point))))
+      (unless (eq beg end)
+        (elpy-shell--flash-and-message-region beg end)
         (elpy-shell--with-maybe-echo
          (python-shell-send-string (python-shell-buffer-substring beg end)))))
-  (python-nav-forward-statement))
+    (python-nav-forward-statement)))
 
 (defun elpy-shell-send-top-statement-and-step ()
   "Send the current or next top-level statement to the Python shell and step.
