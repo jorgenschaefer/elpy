c3be3c8971cd0ceca61a23a5e8f4bb2a02675336
Sean Wang <sean@decrypted.org>
2019-06-27 20:06:27 -0700

    Added a test case to ensure pyproject is read for black.

--- elpy/tests/test_black.py
+++ elpy/tests/test_black.py
@@ -39,3 +39,17 @@ class BLACKTestCase(BackendTestCase):
     def _assert_format(self, src, expected):
         new_block = blackutil.fix_code(src, os.getcwd())
         self.assertEqual(new_block, expected)
+
+    def test_should_read_options_from_pyproject_toml(self):
+        with open('pyproject.toml', 'w') as f:
+            f.write('[tool.black]\nline-length = 10')
+
+        self.addCleanup(os.remove, 'pyproject.toml')
+
+        testdata = [('x=       123\n', 'x = 123\n'),
+                    ('x=1; \ny=2 \n', 'x = 1\ny = 2\n'),
+                    ('x, y, z, a, b, c = 123, 124, 125, 126, 127, 128',
+                     'x, y, z, a, b, c = (\n    123,\n    124,\n    125,'
+                     '\n    126,\n    127,\n    128,\n)\n')]
+        for src, expected in testdata:
+            self._assert_format(src, expected)
