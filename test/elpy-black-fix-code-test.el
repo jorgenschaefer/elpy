30222361ea97ab9322221849491f05061821ffe9
galaunay <gaby.launay@protonmail.com>
2019-10-30 10:15:10 +0100

    Fix test following black version change

--- test/elpy-black-fix-code-test.el
+++ test/elpy-black-fix-code-test.el
@@ -104,13 +104,14 @@
     (elpy-testcase ()
        ;; create pyproject.toml
        (with-current-buffer (find-file-noselect "pyproject.toml")
-         (insert "[tool.black]\nline-length = 10\nexclude = '''\n'''\n")
+         (insert "[tool.black]\nline-length = 20\nexclude = '''\n'''\n")
          (save-buffer))
        (set-buffer-string-with-point
         "_|_print(1, 2, 3, 4, 5, 6, 7)"
         "x, y, z, a, b, c = 1, 2, 3, 4, 5, 6"
         )
        (elpy-black-fix-code)
+       (delete-file "pyproject.toml")
        (should
         (buffer-be
          "_|_print("
@@ -130,5 +131,4 @@
          "    5,"
          "    6,"
          ")"))
-       (delete-file "pyproject.toml")
        )))
