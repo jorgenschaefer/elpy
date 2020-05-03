07bf67e1867d3c6cc7d383e403e731a469785ac2
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2020-05-03 11:06:37 +0100

    Cover additional exceptions that jedi can throw

--- elpy/jedibackend.py
+++ elpy/jedibackend.py
@@ -372,7 +372,7 @@ def get_annotation(proposal):
     if proposal.type in ("instance", "statement", "param", "function"):
         try:
             hint = " ({})".format(proposal.get_type_hint())
-        except AttributeError:
+        except (AttributeError, TypeError, NotImplementedError):
             hint = ""
         return proposal.type + hint
     else:
